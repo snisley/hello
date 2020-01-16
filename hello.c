6875b8726085bc84b718041e0adca1492752e1a0
Dbudhathoki <54649568+Dbudhathoki@users.noreply.github.com>
2020-01-16 14:19:45 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -14,6 +14,9 @@ printf("Electrical Engineering");
 printf("Osama Al Sabahi\n");
 
 printf("Electrical Engineering")
+printf("Dinesh Budhathoki\n");
+
+printf("Comp sci and Mathematics")
      
 
 return 0;}
